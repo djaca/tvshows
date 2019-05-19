7537116a33cc38cc5fd45d20107dcd44dffac8c5
Aleksandar Savanovic <djaca85@gmail.com>
2019-05-19 19:43:19 +0200

    Move format bytes function to utilities

--- src/renderer/store/modules/Torrent.js
+++ src/renderer/store/modules/Torrent.js
@@ -1,24 +1,9 @@
 import torrentStream from 'torrent-stream'
+import { formatBytes } from '@/utilities'
 const { app } = require('electron').remote
 
 let engine = null
 let timer = null
-
-function formatBytes (a, round = false) {
-  if (a === 0) {
-    return '0 Bytes'
-  }
-  let c = 1024
-  let d = 2
-  let e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  let f = Math.floor(Math.log(a) / Math.log(c))
-
-  if (round) {
-    return Math.floor(parseFloat((a / Math.pow(c, f)).toFixed(d))) + ' ' + e[f]
-  }
-
-  return parseFloat((a / Math.pow(c, f)).toFixed(d)) + ' ' + e[f]
-}
 
 const state = {
   torrent: null,
